c56abba331a53fea5a661dcfa1a67f8b972a4479
alvindimas05 <alvin.dimas.praditya@gmail.com>
2026-06-30 10:31:33 +0700

    ci: astro deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+    site: process.env.NODE_ENV === 'production' ? 'https://luneflu.github.io' : undefined,
+    base: process.env.NODE_ENV === 'production' ? '/Calina' : undefined,
     vite: {
         plugins: [tailwindcss()],
     },
